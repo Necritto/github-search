7498be462fb514d98d66d244f61d8bf57768646d
Necritto <onetopyr@mail.ru>
2020-04-03 22:42:35 +0300

    Feature: home page [ref:no-ref]

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,10 +1,18 @@
 import React from 'react'
+import { Search } from '../components/Search'
+import { Card } from '../components/Card'
 
 export const Home = () => {
 
   return (
-    <div>
-      <h1>Home page</h1>
-    </div>
+    <>
+      <Search />
+
+      <div className="row">
+        <div className="col-sm-4 mb-4">
+          <Card />
+        </div>
+      </div>
+    </>
   )
 }
