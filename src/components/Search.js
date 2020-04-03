970c05ae5e299e94c01e32a1b1113c080ba43d36
Necritto <onetopyr@mail.ru>
2020-04-03 22:40:55 +0300

    Feature: search component [ref:no-ref]

---
+++ src/components/Search.js
@@ -0,0 +1,32 @@
+import React, { useContext, useState } from 'react'
+import { AlertContext } from '../context/alert/alertContext'
+
+export const Search = () => {
+  const [value, setValue] = useState('')
+  const { show } = useContext(AlertContext)
+
+  const onSubmit = event => {
+    if (event.key !== 'Enter') {
+      return
+    }
+
+    if (value.trim()) {
+      console.log(value);
+    } else {
+      show('Enter user details!')
+    }
+  }
+
+  return (
+    <div className="form-group">
+      <input
+        type="text"
+        className="form-control"
+        placeholder="Enter username..."
+        onKeyPress={onSubmit}
+        value={value}
+        onChange={event => setValue(event.target.value)}
+      />
+    </div>
+  )
+}
