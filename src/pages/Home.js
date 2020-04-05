797f712d6c2a6ab6c4b5f8758be1b68ac913818a
Necritto <onetopyr@mail.ru>
2020-04-05 21:31:58 +0300

    Feature: user mapping [ref:no-ref]

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,17 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Search } from '../components/Search'
 import { Card } from '../components/Card'
+import { GithubContext } from '../context/github/githubContext'
 
 export const Home = () => {
+  const { loading, users } = useContext(GithubContext)
 
   return (
     <>
       <Search />
-
       <div className="row">
-        <div className="col-sm-4 mb-4">
-          <Card />
-        </div>
+
+        {
+          loading
+            ? <p className="text-center">Loading...</p>
+            : users.map(user => (
+              <div className="col-sm-4 mb-4" key={user.id}>
+                <Card user={user} />
+              </div>
+            ))
+        }
       </div>
     </>
   )
